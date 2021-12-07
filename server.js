16a478609e3652b4878a9283db347671bb0a7d07
Martin <martintomasvivancomella@gmail.com>
2021-12-07 12:13:47 -0300

    Arreglando server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const { apply } = require('core-js/fn/reflect');
 const express = require('express')
-const port = process.env.PORT || 8080;
+const path = require('path');
 
-app.use(express.static(__dirname + "/dist/"));
-app.get(/.*/,function(req,res) {
-    res.sendfile(__dirname + "/dist/index.html");
-});
+const app = express();
+
+app.use('/', serveStatic(path.join(__dirname,'/dist')))
+
+const port = process.env.PORT || 8080;
 app.listen(port);
 
 console.log("Server started...")
